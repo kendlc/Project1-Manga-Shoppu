3eb6b479458c41f8e6f6f2a6751231d002310d89
kendlc <kennethcdlc@gmail.com>
2022-04-19 10:28:15 +1000

    cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -15,23 +15,6 @@ ActiveRecord::Schema.define(version: 2022_04_12_011940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authors", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.text "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "genres_mangas", id: false, force: :cascade do |t|
-    t.integer "genre_id"
-    t.integer "manga_id"
-  end
-
   create_table "mangas", force: :cascade do |t|
     t.text "title"
     t.text "image"
